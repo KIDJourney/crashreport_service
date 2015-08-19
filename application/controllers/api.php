eb17c38d5b15f57cafc0b14739209ab5ee3c4329
KIDJourney <kingdeadfish@qq.com>
2015-08-18 23:20:31 -0400

    modify global setting

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -1,9 +1,35 @@
 <?php
-class Test extends CI_Controller {
+class Api extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->library('auth_lib');
+    }
 
     public function index()
     {
         echo 'Hello World!';
     }
+
+    public function user_login()
+    {
+        $result = array('status'=>0);
+        if ($this->input->post('username') && $this->input->post('password')) {
+            $username = $this->input->post('username');
+            $passwd = $this->input->post('password');
+            $this->auth_lib->init_lib('users', 'user_login', 'user_passwd', 'user');
+            if ($this->auth_lib->login($username, $passwd)) {
+                $result['status'] = 1;
+            }
+        }
+        echo json_encode($result);
+
+    }
+
+    public function repairer_login()
+    {
+
+
+    }
 }
-?>

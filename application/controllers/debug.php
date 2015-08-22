19d725933b8a5e951f0696da2b421e54b6bc8f2c
KIDJourney <kingdeadfish@qq.com>
2015-08-22 13:34:22 -0400

    finish upload demo

--- application/controllers/debug.php
+++ application/controllers/debug.php
@@ -3,6 +3,7 @@ class Debug extends CI_Controller{
     function __construct(){
         parent::__construct();
         $this->load->library('session');
+        $this->load->helper(array('url','form'));
     }
 
     function index()
@@ -17,12 +18,42 @@ class Debug extends CI_Controller{
         echo $this->session->userdata('type');
     }
 
-    function setsession($value){
+    function setsession($value)
+    {
         $this->session->set_userdata($value);
     }
 
-    function save_file(){
+    function save_file()
+    {
+        $config['upload_path'] = 'picture';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['encrypt_name'] = true;
 
+        $this->load->library('upload' , $config);
+
+        if (!$this->upload->do_upload()){
+            print_r($this->upload->display_errors());
+            echo <<<EOF
+<html>
+<head>
+<title>Upload Form</title>
+</head>
+<body>
+EOF;
+            echo form_open_multipart('debug/save_file');
+            echo <<<EOF
+<input type="file" name="userfile" size="20" />
+<br /><br />
+<input type="submit" value="upload" />
+</form>
+</body>
+</html>
+EOF;
+        } else {
+            print_r($this->upload->data());
+
+            echo "Upload Success";
+        }
 
     }
 }

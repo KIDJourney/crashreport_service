2592476b1a7854fe48803318905d65ce1f067e25
KIDJourney <kingdeadfish@qq.com>
2015-08-31 22:54:02 -0400

    add relative database opeartor

--- application/models/api_model.php
+++ application/models/api_model.php
@@ -12,11 +12,21 @@
             return $this->db->insert('report',$insert_data);
         }
 
-        public function list_report($page)
+        public function list_report($page=1)
         {
             if ($page <= 0)
                 $page = 1;
             return $this->db->get('report',$page*10 , ($page - 1) * 10);
+        }
+
+        public function add_user($user_data)
+        {
+            return $this->db->insert('users',$user_data);
+        }
+
+        public function check_report($report_id)
+        {
+            return $this->db->get_where('report',array('id'=>$report_id));            
         }
 
 }

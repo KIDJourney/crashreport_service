e90373e1515583e33a67a2c5f613f98c75233c56
KIDJourney <kingdeadfish@qq.com>
2015-09-12 11:25:03 -0400

    refact function struct , fix tel validate

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -26,7 +26,7 @@ class Api extends CI_Controller {
             $this->auth_lib->init_lib('repairer', 'repairer_login', 'repairer_passwd', 'repairer');
             if ($info = $this->auth_lib->login($username, $passwd)) {
                 $result['status'] = "1";
-                $result['info'] = $info ;
+                $result['info'] = $info;
             }
         } else {
             $result['error'] = "username or password incorrect";
@@ -62,7 +62,7 @@ class Api extends CI_Controller {
 //            $this->error_message("You have not logged in");
 //        }
         $this->auth_lib->logoff();
-        $this->output->set_output(json_encode(array('status'=>'1')));
+        $this->output->set_output(json_encode(array('status' => '1')));
     }
 
     public function user_register()
@@ -84,25 +84,29 @@ class Api extends CI_Controller {
             }
         }
         if (!$flag) {
-            $result['error'] = "Some argument is missed";
+            $this->error_message("Some argument is missed");
+            return;
+        }
+
+        if (!preg_match('/^[A-Za-z][A-za-z0-9]{5,31}/', $user_data['user_login'])) {
+            $this->error_message("user name illegal");
+            return;
+        }
+        if (!preg_match('/^[0-9]{11}/', $user_data['user_tel'])) {
+            $this->error_message("user tel illegal");
+            return;
+        }
+
+        $this->api_model->add_user($user_data);
+        if ($this->db->_error_message()) {
+            $result['status'] = 0;
+            $result['error'] = $this->db->_error_message();
             $this->output->set_output(json_encode($result));
         } else {
-            if (!preg_match('/^[A-Za-z][A-za-z0-9]{5,31}&/', $user_data['user_login'])) {
-                $result['status'] = 0;
-                $result['error'] = "user name illegal";
-                $this->output->set_output(json_encode($result));
-            } else {
-                $this->api_model->add_user($user_data);
-                if ($this->db->_error_message()) {
-                    $result['status'] = 0;
-                    $result['error'] = $this->db->_error_message();
-                    $this->output->set_output(json_encode($result));
-                } else {
-                    $result['status'] = 1;
-                    $this->output->set_output(json_encode($result));
-                }
-            }
+            $result['status'] = 1;
+            $this->output->set_output(json_encode($result));
         }
+
     }
 
 
@@ -110,80 +114,77 @@ class Api extends CI_Controller {
     {
         $result = array('status' => "0");
         if (!$this->auth_lib->check_login()) {
-            $result['status'] = "0";
-            $result['error'] = "You have to login";
+            $this->error_message("You have to login");
+            return;
+        }
+
+        $upload_config = array(
+            'upload_path'   => 'picture',
+            'encrypt_name'  => true,
+            'allowed_types' => 'jpg|png',
+        );
+
+        $this->load->library('upload', $upload_config);
+
+        $required = array(
+            'report_pos'  => '',
+            'report_info' => '',
+            'report_type' => '',
+        );
+
+        $avaliable = True;
+
+        foreach ($required as $key => $value) {
+            if (!$this->input->post($key)) {
+                $avaliable = False;
+            } else {
+                $required[$key] = $this->input->post($key);
+            }
+        }
+        if (!$avaliable) {
+            $this->error_message("You have missed some arguments");
+            return;
+        }
+
+        if (!$this->upload->do_upload('picture')) {
+            $this->error_message($this->upload->display_errors());
+            return;
+
+        }
+
+        $file_data = $this->upload->data();
+        $file_name = $file_data['file_name'];
+
+        $required['report_picurl'] = $file_name;
+        $required['report_reporter'] = $this->auth_lib->check_login();
+
+        if ($this->api_model->add_report($required)) {
+            $result['status'] = 'success';
             $this->output->set_output(json_encode($result));
         } else {
-            $upload_config = array(
-                'upload_path'   => 'picture',
-                'encrypt_name'  => true,
-                'allowed_types' => 'jpg|png',
-            );
-
-            $this->load->library('upload', $upload_config);
-
-            $required = array(
-                'report_pos'  => '',
-                'report_info' => '',
-                'report_type' => '',
-            );
-            $avaliable = True;
-            foreach ($required as $key => $value) {
-                if (!$this->input->post($key)) {
-                    $avaliable = False;
-                } else {
-                    $required[$key] = $this->input->post($key);
-                }
-            }
-            if (!$avaliable) {
-                $result['status'] = "0";
-                $result['error'] = "You have missed some arguments";
-                $this->output->set_output(json_encode($result));
-            } else {
-
-                if (!$this->upload->do_upload('picture')) {
-                    $result['status'] = "0";
-                    $result['error'] = $this->upload->display_errors();
-                    echo $this->upload->display_errors();
-                    $this->output->set_output(json_encode($result));
-                } else {
-                    $file_data = $this->upload->data();
-                    $file_name = $file_data['file_name'];
-
-                    $required['report_picurl'] = $file_name;
-                    $required['report_reporter'] = $this->auth_lib->check_login();
-
-                    if ($this->api_model->add_report($required)) {
-                        $result['status'] = 'success';
-                        $this->output->set_output(json_encode($result));
-                    } else {
-                        $result['status'] = '0';
-                        $result['error'] = "databases error";
-                        $this->output->set_output(json_encode($result));
-                    }
-                }
-            }
+            $this->error_message("databases error");
         }
     }
 
-    public function list_report($page=1)
+    public function list_report($page = 1)
     {
         if (isset($report_id) && !is_numeric($report_id)) {
             $this->error_message("You have no access right");
-        } else {
-            if (!is_numeric($page)) {
-                $this->error_message("incorrect page number");
-            } else {
-                $report_list = ($this->api_model->list_report($page));
-                $this->output->set_output(json_encode($report_list));
-            }
+            return;
         }
 
+        if (!is_numeric($page)) {
+            $this->error_message("incorrect page number");
+        } else {
+            $report_list = ($this->api_model->list_report($page));
+            $this->output->set_output(json_encode($report_list));
+        }
     }
+
 
     public function list_my_report()
     {
-        if (!$this->check_login()){
+        if (!$this->check_login()) {
             $this->error_message("You haven't logged in");
         } else {
             $suid = $this->check_login();
@@ -194,8 +195,8 @@ class Api extends CI_Controller {
 
     public function list_repairer_accept()
     {
-        if (!$this->check_access('repairer')){
-            $this->output->set_output(json_encode(array('status'=>'0','error'=>'You have not logged in')));
+        if (!$this->check_access('repairer')) {
+            $this->output->set_output(json_encode(array('status' => '0', 'error' => 'You have not logged in')));
         } else {
             $suid = $this->check_login();
             $reports = $this->api_model->list_repairer_report($suid);
@@ -246,30 +247,38 @@ class Api extends CI_Controller {
 
     public function add_comment()
     {
-        if (!$this->check_access('user')){
+        if (!$this->check_access('user')) {
             $this->error_message("You don't have access");
-        } else {
-            $required = array('report_id'=>'','comment_content'=>'');
-            $flag = 1;
-            foreach($required as $key=>$value){
-                if($this->input->post($key)){
-                    $required[$key] = $this->input->post($key);
-                } else {
-                    $flag = 0;
-                    break;
-                }
+            return;
+        }
+
+        $required = array('report_id' => '', 'comment_content' => '');
+        $flag = 1;
+        foreach ($required as $key => $value) {
+            if ($this->input->post($key)) {
+                $required[$key] = $this->input->post($key);
+            } else {
+                $flag = 0;
+                break;
             }
-            if ($flag){
-                if($this->api_model->add_comment($this->check_login(),
-                        $required['report_id'],$required['comment_content'])){
-                    $this->output->set_output(json_encode(array('status'=>'1')));
-                } else {
-                    $this->error_message("Databases Error");
-                }
+        }
+        if ($flag) {
+            if ($this->api_model->add_comment($this->check_login(),
+                $required['report_id'], $required['comment_content'])
+            ) {
+                $this->output->set_output(json_encode(array('status' => '1')));
+            } else {
+                $this->error_message("Databases Error");
             }
         }
     }
 
+    public function debug()
+    {
+        $this->output->set_output("blablabla");
+        return;
+        $this->output->set_output("I don't want to blabla");
+    }
 
 
     private function check_login()

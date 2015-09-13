ae594321156d140d5d96f0cbe3e86ff8ef363d31
KIDJourney <kingdeadfish@qq.com>
2015-09-13 11:36:09 -0400

    add fund function

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -20,26 +20,35 @@ class Manage extends CI_Controller {
 
     public function index()
     {
-        echo "This is index";
+        $username = $this->session->userdata('username');
+        $recent_report = $this->manage_model->get_recent_report();
+
+        $this->load->view('manage/dashboard',array('username'=>$username,"reports"=>$recent_report));
     }
 
     public function login()
     {
         if (!($this->input->post('username') && $this->input->post('password'))) {
             $this->load->view('manage/login');
+            return;
         }
 
         $username = $this->input->post('username');
         $password = $this->input->post('password');
         $this->auth_lib->init_lib('manager', 'manager_login', 'manager_passwd', 'manager');
         if ($this->auth_lib->login($username, $password)){
+            $this->session->set_userdata(array('username'=>$username));
             redirect(base_url('manage'));
         } else {
             $this->load->view('manage/login',array('error'=>'true'));
         }
-
     }
 
+    public function logoff()
+    {
+        $this->auth_lib->logoff();
+        redirect('manage/login');
+    }
 
     private function check_access()
     {
@@ -49,6 +58,11 @@ class Manage extends CI_Controller {
         } else {
             return False;
         }
+    }
+
+    public function debug()
+    {
+        print_r($this->manage_model->get_recent_report());
     }
 
 

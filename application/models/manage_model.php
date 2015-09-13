5aebe59ee9c446eb71255063d8a30e69b999ff98
KIDJourney <kingdeadfish@qq.com>
2015-09-13 17:30:18 -0400

    add report count query

--- application/models/manage_model.php
+++ application/models/manage_model.php
@@ -1,4 +1,5 @@
 <?php
+
 class Manage_model extends CI_Model {
 
     function __construct()
@@ -9,13 +10,37 @@ class Manage_model extends CI_Model {
 
     public function  get_recent_report()
     {
-        $this->db->order_by("report_createat desc");
-        $this->db->where('report_status',0);
-        return $this->db->get('report',10,0)->result();
+        $sql = <<<SQL
+    select r.id , p.pos_name as report_pos , t.type_name as report_type , r.report_info , r.report_picurl , u.user_nickname as report_reporter
+    FROM report r
+    LEFT JOIN position p
+    ON r.report_pos = p.id
+    LEFT JOIN report_type t
+    ON r.report_type = t.id
+    LEFT JOIN users u
+    ON r.report_reporter = u.id
+    where r.report_status = 0
+SQL;
+        $query = $this->db->query($sql);
+        return $query ? $query->result() : false;
     }
-    //SELECT report.id, report.report_pos, report.report_type, report.report_info, report.report_picurl, users.user_nickname AS report_reporter
-    //FROM report
-    //LEFT JOIN users
-    //on report.report_reporter = users.id
 
+    public function get_report_count()
+    {
+        function filter(&$sql_result, $key)
+        {
+        }
+
+        $time = 24 * 60 * 60;
+
+        $report        = $this->db->query("SELECT COUNT(*) as num FROM report where
+            (report.report_status = 0 and (current_timestamp() - report.report_createat < $time))")->result();
+        $report_accept = $this->db->query("SELECT COUNT(*) as num FROM report where
+            (report.report_status = 1 and (current_timestamp() - report.report_createat < $time))")->result();
+        $report_finish = $this->db->query("SELECT COUNT(*) as num FROM report where
+            (report.report_status = 2 and (current_timestamp() - report.report_createat < $time))")->result();
+        $report_all    = $this->db->count_all('report');
+        return array("report"=>$report[0]->num , "report_accept" =>$report_accept[0]->num , "report_finish" => $report_finish[0]->num , "report_all"=>$report_all);
+
+    }
 }

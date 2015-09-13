2cd63bc68aa4b4e81ab1163ac626aa216141ea6e
KIDJourney <kingdeadfish@qq.com>
2015-09-13 11:36:33 -0400

    add fund db op

--- application/models/manage_model.php
+++ application/models/manage_model.php
@@ -7,4 +7,15 @@ class Manage_model extends CI_Model {
         $this->load->database();
     }
 
+    public function  get_recent_report()
+    {
+        $this->db->order_by("report_createat desc");
+        $this->db->where('report_status',0);
+        return $this->db->get('report',10,0)->result();
+    }
+    //SELECT report.id, report.report_pos, report.report_type, report.report_info, report.report_picurl, users.user_nickname AS report_reporter
+    //FROM report
+    //LEFT JOIN users
+    //on report.report_reporter = users.id
+
 }
